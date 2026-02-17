17251bb2d128f725aeb598ccc4af968eeb3b9ca3
Hüseyin Dinçbay <hdincbay3@gmail.com>
2026-02-17 23:14:55 +0300

    Implemented structural revisions across the WebUI project.

---
+++ PetMonitoring/PetMonitoring.WebUI/Controllers/DeviceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetMonitoring.WebUI.Controllers
+{
+    public class DeviceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
